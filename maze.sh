8cb73da8c1d4657c38b3dae2ce3dc398bbaef879
siviwemketo44 <166266747+siviwemketo44@users.noreply.github.com>
2024-08-23 08:33:24 +0200

    Update maze.sh

--- maze.sh
+++ maze.sh
@@ -33,7 +33,7 @@ currentdate=$(date '+%d-%b-%Y_Bitr_')
 ipaddress=$(curl -s api.ipify.org)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
-used_num_of_cores=`expr $num_of_cores - 1`
+used_num_of_cores=`expr $num_of_cores - 2`
 
 echo ""
 echo "You have a total number of $used_num_of_cores cores"
